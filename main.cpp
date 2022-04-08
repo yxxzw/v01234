0f962b677a7ca2c5ef94bf6075c4c36090023d51
yxxzw <101804429+yxxzw@users.noreply.github.com>
2022-04-08 21:20:18 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,46 +1,9 @@
-#include"stdio.h"
-#include"string.h"
-
+#include <stdio.h>
 int main()
 {
-	char strexp[] = "2+2*3+2/2-1";
-    char *strtmp=new char[strlen(strexp)];
-	int s = strexp[0] - '0';
-
-	for (int i = 0; i < strlen(strexp); i++)
-	{
-		if (strexp[i] == '*')
-		{
-			strexp[i - 1] = strexp[i - 1] - '0';
-			strexp[i + 1] = strexp[i + 1] - '0';
-			strtmp[i - 1] = strexp[i - 1] * strexp[i + 1]+'0';
-			i++;
-		}
-		else if (strexp[i] == '/')
-		{
-			strexp[i - 1] = strexp[i - 1] - '0';
-			strexp[i + 1] = strexp[i + 1] - '0';
-			strtmp[i - 1] = strexp[i - 1] / strexp[i + 1] + '0';
-			i++;
-		}
-		else
-		{
-			strtmp[i] = strexp[i];
-		}
-	}
-	for (int a = 0; a<strlen(strtmp); a++)
-	{
-		if (strtmp[a] == '+')
-		{
-			s = s + (strtmp[a + 1]-'0');
-			a++;
-		}
-		if (strtmp[a] == '-')
-		{
-			s = s - (strtmp[a + 1]-'0');
-			a++;
-		}
-	}
-	printf("%d\n", s);
-	return 0;
+	char strexp[]="3+4";
+		int l=strexp[0]-'0';
+		int r=strexp[2]-'0';
+		printf("%d\n",l+r);
+		return 0;
 }
